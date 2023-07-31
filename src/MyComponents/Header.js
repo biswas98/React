68cc738538236b73f3d2b695c571c56bd315741c
biswas98 <biswasbiplab98@gmail.com>
2023-07-31 21:17:05 +0530

    minor code change

--- src/MyComponents/Header.js
+++ src/MyComponents/Header.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 export default function Header() {
   return (
-    <ul>
+    <>
+      <ul>
         <li>Home</li>
         <li>Contact</li>
-    </ul>
+      </ul>
+    </>
   );
 }
 
