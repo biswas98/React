5dfdc3a4029ef973b6eb9adfcdfdcdc62505d2ea
biswas98 <biswasbiplab98@gmail.com>
2023-08-18 15:06:36 +0530

    replacing button with anchor tag

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [incPresnt, setInc] = useState(0);
+  const [decPresnt, setDec] = useState(0);
+
+  const increamentFunction = () => setInc(incPresnt + 5);
+  const decreamentFunction = () => setDec(decPresnt - 5);
+
   return (
     <>
       <div className="container">
         <h1 className="heading">Click</h1>
 
         <div>
-          <a href="#">-</a>
-
+          <button>-</button>
+		
           <input />
 
-          <a href="#">+</a>
+          <button>+</button>
         </div>
       </div>
     </>
