e2c03cea1c26cabbcc80e8ddf3bd42a129698787
biswas98 <biswasbiplab98@gmail.com>
2023-08-21 01:52:23 +0530

    implementing axios and trying different APIs

--- src/MyComponents/API/API.jsx
+++ src/MyComponents/API/API.jsx
@@ -1,21 +1,33 @@
 import React, { useEffect } from 'react';
+import axios, { AxiosError } from 'axios';
 
 import './apiStyle.css';
 
-const weatherAPI = async () =>{
-    
-    const url = 'https://animechan.xyz/api/quotes'
-
-    let response = await fetch(url);
-    response = await response.json();
-}
+const API = 'https://cat-fact.herokuapp.com/facts';
 
 export default function () {
-  return (
-    <div className='apiContainer'>
-        <div className="quotes">
-            
+
+    const weatherAPI = async () => {
+        try {
+            let response = await axios.get(API);
+            console.log('successfull');
+            // console.log(response.data[0].text);
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+
+    useEffect(() => {
+        weatherAPI();
+    }, [])
+
+    return (
+        <div className='apiContainer'>
+            <div className="quotes">
+
+            </div>
         </div>
-    </div>
-  )
+    )
 }
