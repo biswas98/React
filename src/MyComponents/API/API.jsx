a163006a921410360e943fa2ff85ca503f3fd710
biswas98 <biswasbiplab98@gmail.com>
2023-08-20 15:25:21 +0530

    api fetch

--- src/MyComponents/API/API.jsx
+++ src/MyComponents/API/API.jsx
@@ -2,16 +2,19 @@ import React, { useEffect } from 'react';
 
 import './apiStyle.css';
 
-const weatherAPI = () =>{
+const weatherAPI = async () =>{
+    
+    const url = `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${API_KEY}&q=${place}`
 
+    let response = await fetch(url);
+    response = await response.json();
 }
-
 
 export default function () {
   return (
     <div className='apiContainer'>
         <input type="text" />
-        <button>Click</button>
+        <button onClick={weatherAPI}>Click</button>
     </div>
   )
 }
