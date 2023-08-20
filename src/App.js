d8747db1e6221a7a87bfece166d357c42bc30e27
biswas98 <biswasbiplab98@gmail.com>
2023-08-20 15:10:28 +0530

    added API component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 
 import IncDec from "./MyComponents/PlusMinus/IncDec";
+import API from "./MyComponents/API/API";
+
 
 function App() {
   return (
     <>
-      <IncDec />
+      {/* <IncDec /> */}
+      <API />
     </>
   );
 }
---
+++ src/MyComponents/API/API.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './apiStyle.css';
+
+export default function () {
+  return (
+    <div className='apiContainer'>
+        <input type="text" />
+        <button>Click</button>
+    </div>
+  )
+}
