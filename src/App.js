d00eb49a314365a92d0d50156bb2940182e04b49
Biplab Biswas <biswasbiplab98@gmail.com>
2023-08-30 11:24:51 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 
 import IncDec from "./MyComponents/PlusMinus/IncDec";
 import API from "./MyComponents/API/API";
-import Test from './MyComponents/API/Test';
+import Test from "./MyComponents/API/Test";
 
 function App() {
   return (
