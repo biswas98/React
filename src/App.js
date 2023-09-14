99feb2f33361eb5263e2d6976bb7893d8b554b2d
Biplab Biswas <biswasbiplab98@gmail.com>
2023-09-14 23:00:06 +0530

    api two removed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import "./App.css";
 import IncDec from "./MyComponents/PlusMinus/IncDec";
 import API from "./MyComponents/API/API";
 import Test from "./MyComponents/API/Test";
-import ApiTwo from "./MyComponents/API/ApiTwo";
 
 function App() {
   return (
