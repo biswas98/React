371a77cad1cb0ccca2c189c624fd2a4d7196d4fb
Biplab Biswas <biswasbiplab98@gmail.com>
2023-09-18 22:35:02 +0530

    BallTriangle

--- src/MyComponents/Weather/index.jsx
+++ src/MyComponents/Weather/index.jsx
@@ -1,8 +1,22 @@
 import React from "react";
+import { BallTriangle } from "react-loader-spinner";
 // require("dotenv").config();
 
 export default function Weather() {
   //   console.log(process.env.SECRET_KEY1);
   //need to implement .env
-  return <div></div>;
+  return (
+    <div>
+      <BallTriangle
+        height={100}
+        width={100}
+        radius={5}
+        color="#4fa94d"
+        ariaLabel="ball-triangle-loading"
+        wrapperClass={{}}
+        wrapperStyle=""
+        visible={true}
+      />
+    </div>
+  );
 }
