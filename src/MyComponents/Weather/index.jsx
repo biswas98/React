fa4b1592eb0846f10009891d4a0ea9680f50b6a0
Biplab Biswas <biswasbiplab98@gmail.com>
2023-09-18 22:33:28 +0530

    installing loader package

--- src/MyComponents/Weather/index.jsx
+++ src/MyComponents/Weather/index.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-require("dotenv").config();
+// require("dotenv").config();
 
 export default function Weather() {
-  console.log(process.env.SECRET_KEY1);
+  //   console.log(process.env.SECRET_KEY1);
   //need to implement .env
   return <div></div>;
 }
