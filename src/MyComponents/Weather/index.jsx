9c92cfa2a5dcc74f489d2236b7aa8af0198ff158
Biplab Biswas <biswasbiplab98@gmail.com>
2023-09-18 22:44:01 +0530

    changing loader style to custome styles

--- src/MyComponents/Weather/index.jsx
+++ src/MyComponents/Weather/index.jsx
@@ -2,41 +2,45 @@ import React from "react";
 import { BallTriangle, FidgetSpinner, Puff } from "react-loader-spinner";
 // require("dotenv").config();
 
+import "./styles.css";
+
 export default function Weather() {
   //   console.log(process.env.SECRET_KEY1);
   //need to implement .env
   return (
     <div>
-      <BallTriangle
-        height={100}
-        width={100}
-        radius={5}
-        color="#4fa94d"
-        ariaLabel="ball-triangle-loading"
-        wrapperClass={{}}
-        wrapperStyle=""
-        visible={true}
-      />
-      <FidgetSpinner
-        visible={true}
-        height="80"
-        width="80"
-        ariaLabel="dna-loading"
-        wrapperStyle={{}}
-        wrapperClass="dna-wrapper"
-        ballColors={["#ff0000", "#00ff00", "#0000ff"]}
-        backgroundColor="#F4442E"
-      />
-      <Puff
-        height="80"
-        width="80"
-        radius={1}
-        color="#4fa94d"
-        ariaLabel="puff-loading"
-        wrapperStyle={{}}
-        wrapperClass=""
-        visible={true}
-      />
+      <div className="loaderContainer">
+        <BallTriangle
+          height={100}
+          width={100}
+          radius={5}
+          color="#4fa94d"
+          ariaLabel="ball-triangle-loading"
+          wrapperClass={{}}
+          wrapperStyle=""
+          visible={true}
+        />
+        <FidgetSpinner
+          visible={true}
+          height="80"
+          width="80"
+          ariaLabel="dna-loading"
+          wrapperStyle={{}}
+          wrapperClass="dna-wrapper"
+          ballColors={["#ff0000", "#00ff00", "#0000ff"]}
+          backgroundColor="#F4442E"
+        />
+        <Puff
+          height="80"
+          width="80"
+          radius={1}
+          color="#4fa94d"
+          ariaLabel="puff-loading"
+          wrapperStyle={{}}
+          wrapperClass=""
+          visible={true}
+        />
+      </div>
     </div>
   );
 }
